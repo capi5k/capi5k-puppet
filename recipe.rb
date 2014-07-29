f383dbb7a0ed46829afd3ef8f8fb8874eb0362b1
msimonin <matthieu.simonin@inria.fr>
2014-07-29 09:34:24 +0200

    fix script url.
    M

--- recipe.rb
+++ recipe.rb
@@ -14,9 +14,9 @@ namespace :puppet do
     set :user, "root"
     env = "PUPPET_VERSION=#{puppet_version}"
     env += " #{proxy}"
-    run "#{apt_get_p} install -y rubygems" 
     run "#{apt_get_p} update && #{apt_get_p} install -y curl" 
-    run "#{proxy} curl -L https://raw.github.com/pmorillon/puppet-puppet/master/files/scripts/puppet_install.sh | #{env} sh"
+    run "#{apt_get_p} install -y rubygems" 
+    run "#{proxy} curl -L  https://raw.githubusercontent.com/pmorillon/puppet-puppet/master/extras/bootstrap/puppet_install.sh | #{env} sh"
   end
 
 end
